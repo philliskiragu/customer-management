6daff459d3ec2df0ebcfa9d40453bd3513656d52
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-09-04 18:03:10 +0300

    add routing for auth

--- app/main.js
+++ app/main.js
@@ -1,5 +1,42 @@
 /**
  * Created by PKiragu on 30/08/16.
  */
-var app = angular.module('custApp',[]);
+var app = angular.module('custApp', ['ui.router']);
 
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/dashboard');
+
+    $stateProvider
+
+        .state('dash', {
+            url: '',
+            abstract: true,
+            templateUrl: '/dashboard.html'
+        })
+        .state('dashboard',{
+            url: '/dashboard',
+            templateUrl: 'app/dashboard.html'
+        })
+        .state('auth', {
+            url: '/auth',
+            //controller: 'authCtrl as auth1',
+            views: {
+                //main auth template
+                '': {
+                    templateUrl: 'app/auth/auth.html'
+                },
+
+                //login template
+                'login@auth': {
+                    templateUrl: 'app/auth/login.html'
+                },
+
+                //signup template
+                'signup@auth': {
+                    templateUrl: 'app/auth/signup.html'
+                }
+            }
+        });
+
+});
